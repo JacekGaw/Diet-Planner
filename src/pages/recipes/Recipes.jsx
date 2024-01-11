4d45fdc1065f2f8232e5d95025f6ef2ecc88f02e
JacekGaw <jac.gawlyta@gmail.com>
2024-01-11 12:04:41 +0100

    Change styling, add google fonts and google material icons, add icon to recipe page

--- src/pages/recipes/Recipes.jsx
+++ src/pages/recipes/Recipes.jsx
@@ -15,11 +15,18 @@ const Recipes = () => {
   };
 
   return (
-    <section className="p-5">
-      <header>
-        <h2 className="text-center text-3xl text-white">Recipe book</h2>
+    <section className=" relative">
+      <span className="material-symbols-outlined text-[600px] rotate-12 right-[40px] m-0 absolute opacity-5 z-0 ">
+        menu_book
+      </span>
+
+      <header className="w-full p-2 z-50">
+        <h2 className="text-center text-5xl font-regular z-50 drop-shadow-md">Recipe book</h2>
       </header>
-      <button onClick={handleChangeVisibility} className="bg-dark-green text-white rounded p-2 text-sm mb-5">
+      <button
+        onClick={handleChangeVisibility}
+        className="bg-dark-green text-white rounded p-2 text-sm mb-5"
+      >
         {addRecipeView ? "Close" : "Add recipe"}
       </button>
       {addRecipeView === true ? (
@@ -27,7 +34,10 @@ const Recipes = () => {
       ) : (
         ""
       )}
-      <RecipesList className="flex gap-4 " cardStyle=" w-1/4 bg-white rounded-lg flex flex-col justify-between"/>
+      <RecipesList
+        className="flex gap-4 z-50"
+        cardStyle="z-50 w-1/4 bg-white rounded-xl flex flex-col justify-between border-2 border-dark-green"
+      />
     </section>
   );
 };
